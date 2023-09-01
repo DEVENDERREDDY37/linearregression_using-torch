5fba646a75ad7d8a4b06a9cab69c141880567ee9
M. Devender Reddy <119731972+DEVENDERREDDY37@users.noreply.github.com>
2023-09-01 12:03:08 +0530

    Add files via upload

---
+++ linearreg.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+#model creation
+x_numpy,y_numpy=datasets.make_regression(n_samples=100,n_features=1,noise=20)
+x=torch.from_numpy(x_numpy.astype(np.float32))
+y=torch.from_numpy(y_numpy.astype(np.float32))
+y=y.view(y.shape[0],1)
+n_sample,n_features=x.shape
+input_size=n_features
+output_size=1
+#loss and optimizer
+model=nn.Linear(input_size,output_size)
+criterion=nn.MSELoss()
+lr=0.01
+optimizer=torch.optim.SGD(model.parameters(),lr)
+# training loop
+num_epochs=100
+for epoch in range(num_epochs):
+    #forword pass nad loss
+    y_predicted=model(x)
+    loss=criterion(y_predicted,y)
+    #back ward pass
+    loss.backward()
+    #upadte the weights
+    optimizer.step()
+    optimizer.zero_grad()
+    if (epoch+1)%10 ==0:
+        print(f'epoch:{epoch+1},loss{loss.item():.4f}')
+predicted=model(x).detach()
+plt.plot(x_numpy,y_numpy,'ro')
+plt.plot(x_numpy,predicted,'b')
+plt.show()
